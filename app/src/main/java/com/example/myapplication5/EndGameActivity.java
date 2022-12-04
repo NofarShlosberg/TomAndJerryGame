2e126ed1794e8ec880702dc83d2e55f6f93a3cf0
NofarShlosberg <Nofar740@gmail.com>
2022-12-04 22:11:22 +0200

    Add functionality to exit game and back to main buttons

--- app/src/main/java/com/example/myapplication5/EndGameActivity.java
+++ app/src/main/java/com/example/myapplication5/EndGameActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.button.MaterialButton;
@@ -11,8 +13,6 @@ public class EndGameActivity extends AppCompatActivity {
 
     private MaterialButton end_BTN_backtomain;
     private MaterialButton end_BTN_exitgame;
-    private MaterialTextView lbl_game_over;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +21,29 @@ public class EndGameActivity extends AppCompatActivity {
 
         end_BTN_backtomain = findViewById(R.id.end_BTN_backtomain);
         end_BTN_exitgame = findViewById(R.id.end_BTN_exitgame);
-        lbl_game_over = findViewById(R.id.lbl_game_over);
 
+        end_BTN_backtomain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
 
-
-
+        end_BTN_exitgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
+
+    private void openMainActivity() {
+        Intent myintent = new Intent(this,MainActivity.class);
+        startActivity(myintent);
+    }
+
+
 }
+
+
