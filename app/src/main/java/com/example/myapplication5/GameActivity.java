4f93d51d73316204d37031a7fa96630e24e2a960
NofarShlosberg <Nofar740@gmail.com>
2022-12-05 15:05:03 +0200

    Add connection between Jerry in xml and JAVA Add findviews and initviews functions Add initVisibility function to show only one Jerry

--- app/src/main/java/com/example/myapplication5/GameActivity.java
+++ app/src/main/java/com/example/myapplication5/GameActivity.java
@@ -5,23 +5,45 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
 
-public class GameActivity extends AppCompatActivity{
+public class GameActivity extends AppCompatActivity {
 
     private MaterialButton game_BTN_left;
     private MaterialButton game_BTN_right;
     private ShapeableImageView[] game_IMG_hearts;
+    private ShapeableImageView[]  game_IMG_jerryPos;
     Jerry jerry = new Jerry();
-    private static final String TAG ="jerry pos";
+    private static final String TAG = "jerry pos";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        findViews();
+        initViews();
+        initVisibility();
 
+        game_BTN_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveJerry("left");
+                showRealJerry();
+            }
+        });
+        game_BTN_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveJerry("right");
+            }
+
+        });
+    }
+
+    private void findViews() {
         game_BTN_left = findViewById(R.id.game_BTN_left);
         game_BTN_right = findViewById(R.id.game_BTN_right);
 
@@ -31,19 +53,50 @@ public class GameActivity extends AppCompatActivity{
                 findViewById(R.id.game_IMG_heart1),
         };
 
-        game_BTN_left.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moveJerry("left");
-            }
-        });
-        game_BTN_right.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               moveJerry("right");
-            }
+        game_IMG_jerryPos = new ShapeableImageView[]{
+                findViewById(R.id.game_IMG_jerryPos0),
+                findViewById(R.id.game_IMG_jerryPos1),
+                findViewById(R.id.game_IMG_jerryPos2),
+                findViewById(R.id.game_IMG_jerryPos3),
+                findViewById(R.id.game_IMG_jerryPos4)
+        };
 
-        });
+    }
+    private void initViews() {
+
+        //set life IMG
+        for (int i = 0; i < game_IMG_hearts.length; i++) {
+            Glide
+                    .with(this)
+                    .load(R.drawable.ic_heart)
+                    .into(game_IMG_hearts[i]);
+        }
+
+
+        //set Jerry IMG
+        for (int i = 0; i < game_IMG_jerryPos.length; i++) {
+            Glide
+                    .with(this)
+                    .load(R.drawable.ic_jerry)
+                    .centerInside()
+                    .into(game_IMG_jerryPos[i]);
+        }
+
+    }
+
+    private void initVisibility() {
+        //set Jerry default Visibility
+        game_IMG_jerryPos[0].setVisibility(View.INVISIBLE);
+        game_IMG_jerryPos[1].setVisibility(View.INVISIBLE);
+        game_IMG_jerryPos[2].setVisibility(View.VISIBLE);
+        game_IMG_jerryPos[3].setVisibility(View.INVISIBLE);
+        game_IMG_jerryPos[4].setVisibility(View.INVISIBLE);
+
+        //set hearts default Visibility
+        for (int i = 0; i < game_IMG_hearts.length; i++) {
+            game_IMG_hearts[i].setVisibility(View.VISIBLE);
+        }
+
     }
 
     public void moveJerry(String direction) {
@@ -62,5 +115,8 @@ public class GameActivity extends AppCompatActivity{
         }
     }
 
+    private void showRealJerry() {
+
+    }
 
 }
