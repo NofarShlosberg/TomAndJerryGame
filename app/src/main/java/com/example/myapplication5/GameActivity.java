da476797aadb07641a937319427eb37a1223b064
NofarShlosberg <Nofar740@gmail.com>
2022-12-05 14:20:30 +0200

    Add move Jerry function and arrow buttons functionality

--- app/src/main/java/com/example/myapplication5/GameActivity.java
+++ app/src/main/java/com/example/myapplication5/GameActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication5;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,9 @@ public class GameActivity extends AppCompatActivity{
     private MaterialButton game_BTN_left;
     private MaterialButton game_BTN_right;
     private ShapeableImageView[] game_IMG_hearts;
+    Jerry jerry = new Jerry();
+    private static final String TAG ="jerry pos";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,37 @@ public class GameActivity extends AppCompatActivity{
                 findViewById(R.id.game_IMG_heart2),
                 findViewById(R.id.game_IMG_heart1),
         };
+
+        game_BTN_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveJerry("left");
+            }
+        });
+        game_BTN_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               moveJerry("right");
+            }
+
+        });
     }
+
+    public void moveJerry(String direction) {
+        int currentPos = jerry.getPos();
+        if (direction == "right") {
+            if (currentPos != 4) {
+                jerry.setPos(currentPos + 1);
+            }
+            Log.d(TAG, "jerry pos is " + jerry.getPos());
+        }
+        if (direction == "left") {
+            if (currentPos != 0) {
+                jerry.setPos(currentPos - 1);
+            }
+            Log.d(TAG, "jerry pos is " + jerry.getPos());
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/myapplication5/Jerry.java
+++ app/src/main/java/com/example/myapplication5/Jerry.java
@@ -26,6 +26,6 @@ public class Jerry {
     public void hitTom() {
         --numOfLife;
     }
-    
+
 }
 
